1c707aab0c99baa84485d51c3b6d50604a82714e
Voxelix-7 <ayatrshaalan15@gmail.com>
2026-07-05 20:39:24 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,73 @@
+let productQuantity = document.getElementById("productQuantity");
+let productPrice = document.getElementById("productPrice");
+let productName = document.getElementById("productName");
+let message = document.getElementById("message");
+let saveBtn = document.getElementById("saveBtn");
+let productList = document.getElementById("productList");
+
+let products = [];
+let editIndex = null;
+
+let savedProducts = localStorage.getItem("products");
+if(savedProducts !== null) {
+ products = JSON.parse(savedProducts);
+}
+function save() {
+  localStorage.setItem("products", JSON.stringify(products));
+}
+function displayProducts() {
+  productList.innerHTML = "";
+  products.forEach(function(product, index) {
+    productList.innerHTML += `
+    <div class="product-card">
+      <h3>${product.name}</h3>
+      <p>Price: ${product.price}</p>
+      <p>Quantity: ${product.quantity}</p>
+      <button onclick="editProduct"(${index})>Edit</button>
+      <button onclick="deleteProduct"(${index})>Delete</button>
+    </div>
+    `;
+  });
+}
+saveBtn.addEventListener('click', function() {
+  let product = {
+    name: productName.value,
+    price: productPrice.value,
+    quantity: productQuantity.value
+  };
+
+  if(editIndex === null){
+    products.push(product);
+    message.textContent = "Added product";
+  } else {
+    products[editIndex] = product;
+    editIndex = null;
+    saveBtn.textContent = "save product";
+    message.textContent = "Product updated";
+  }
+
+  save();
+  displayProducts();
+
+  productName.value = "";
+  productPrice.value = "";
+  productQuantity.value = "";
+  
+});
+
+function editProduct(index) {
+  productName.value = products[index].name;
+  productPrice.value = products[index].price;
+  productQuantity.value = products[index].quantity;
+
+  editIndex = index;
+  saveBtn.textContent = "Update product";
+}
+
+function deleteProduct(index) {
+  products.splice(index, 1);
+  save();
+  message.textContent = "Product deleted";
+}
+
+displayProducts();
